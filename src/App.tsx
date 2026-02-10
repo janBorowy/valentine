bde9a04a4091cdacd31bdad67fe0f45c1e37b4b4
Jan Borowy <01168874@pw.edu.pl>
2026-02-10 13:25:30 +0100

    added site

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,112 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
+interface State {
+    query: string;
+    nextState: State | null
+    yesToState: State | null
+    noToState: State | null
+    nextMessage: string | null,
+    image: string,
+}
+
+const last: State = {
+    query: "A no tak, to do zobaczenia",
+    nextState: null,
+    yesToState: null,
+    noToState: null,
+    image: "/static/happy-doggo.gif",
+    nextMessage: "Przecież mamy wspólną kartę"
+}
+
+const whoPays: State = {
+    query: "Ale kto płaci?",
+    nextState: last,
+    yesToState: null,
+    noToState: null,
+    image: "/static/jamnik-zlozone-lapki.jpg",
+    nextMessage: "Przecież mamy wspólną kartę"
+}
+
+const hellYeah: State = {
+    query: "YAY!",
+    nextState: whoPays,
+    yesToState: null,
+    noToState: null,
+    image: "/static/excited-doggo.gif",
+    nextMessage: "Dobra, uspokój się już"
+}
+
+
+const areYouSure: State = {
+    query: "Czy jesteś tego pewna?",
+    nextState: null,
+    yesToState: null,
+    noToState: null,
+    image: "/static/sad-henio.webp",
+    nextMessage: null
+}
+
+const initial: State = {
+    query: "Czy zostaniesz moją walentynką?",
+    nextState: null,
+    yesToState: hellYeah,
+    noToState: areYouSure,
+    image: "/static/jamniczek.jpg",
+    nextMessage: null
+};
+
+areYouSure.noToState = initial
+
 function App() {
+    const [state, setState] = useState(initial)
+    const [yesNoReverse, setYesNoReverse] = useState(false)
+
+    function handleYes() {
+        if (state.image === "/static/sad-henio.webp") {
+            alert("No weź pls")
+            setYesNoReverse(!yesNoReverse)
+            return
+        }
+        setState(state.yesToState!)
+    }
+
+    function handleNo() {
+        setState(state.noToState!)
+        if (yesNoReverse) setYesNoReverse(false)
+    }
+
+    function handleNext() {
+        setState(state.nextState!)
+    }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main-column-container">
+        <div className="content-container">
+            <div className="main-image-container">
+                <img src={state.image} alt="cute-image"/>
+            </div>
+            <span className="query-paragraph">{state.query}</span>
+            {state.nextState === null ?
+                (state.yesToState !== null || state.noToState !== null ? <div
+                    style={{
+                        flexDirection: yesNoReverse ? "row-reverse" : "row"
+                    }}
+                    className="buttons-container">
+                    <button onClick={(e) => handleYes()}>Tak</button>
+                    <button onClick={(e) => handleNo()}>Nie</button>
+                </div> : <div></div>) :
+                <div className="buttons-container">
+                    <button onClick={(e) => handleNext()}>{state.nextMessage}</button>
+                </div>
+            }
+        </div>
+        {state.image === "/static/happy-doggo.gif" &&
+            <div>
+            <span className="additional-text">Literalnie ja w tym momencie:</span>
+            <img width="100px" src="/static/jamnik-w-lozeczku.jpg"/>
+            </div>
+        }
     </div>
   );
 }
